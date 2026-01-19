05f8d957c48e906626defa377062f31a4ddc7acc
Nishant Gurung <xenongrg@gmail.com>
2026-01-20 01:01:11 +0530

    Data PreProcessing

--- lab1_data_preprocessing/data_preprocessing_using_numpy.py
+++ lab1_data_preprocessing/data_preprocessing_using_numpy.py
@@ -22,3 +22,26 @@ print("\nAfter Handling Missing Values:\n", data)
 # Normalization
 normalized_data = (data - data.min(axis=0)) / (data.max(axis=0) - data.min(axis=0))
 print("\nNormalized Data:\n", normalized_data)
+
+
+# ***OUTPUT***
+# Original Data:
+#  [[2.5e+01 5.0e+04]
+#  [3.0e+01 6.0e+04]
+#  [3.5e+01     nan]
+#  [4.0e+01 8.0e+04]
+#  [    nan 9.0e+04]]
+
+# After Handling Missing Values:
+#  [[2.50e+01 5.00e+04]
+#  [3.00e+01 6.00e+04]
+#  [3.50e+01 7.00e+04]
+#  [4.00e+01 8.00e+04]
+#  [3.25e+01 9.00e+04]]
+
+# Normalized Data:
+#  [[0.         0.        ]
+#  [0.33333333 0.25      ]
+#  [0.66666667 0.5       ]
+#  [1.         0.75      ]
+#  [0.5        1.        ]]
--- lab1_data_preprocessing/data_preprocessing_using_pandas.py
+++ lab1_data_preprocessing/data_preprocessing_using_pandas.py
@@ -20,4 +20,30 @@ print("\nAfter Filling Missing Values:\n", df)
 # One-Hot Encoding categorical data
 one_hot = pd.get_dummies(df['City'], prefix='City')
 df_encoded = pd.concat([df, one_hot], axis= 1)
-print("\nAfter Encoding Categorical Data:\n", df_encoded)
+print("\nAfter One-Hot Encoding Categorical Data:\n", df_encoded)
+
+
+# ***OUTPUT***
+# Original DataFrame:
+#      Age   Salary     City
+# 0  25.0  50000.0    Delhi
+# 1  30.0  60000.0   Mumbai
+# 2  35.0      NaN    Delhi
+# 3  40.0  80000.0  Chennai
+# 4   NaN  90000.0   Mumbai
+
+# After Filling Missing Values:
+#      Age   Salary     City
+# 0  25.0  50000.0    Delhi
+# 1  30.0  60000.0   Mumbai
+# 2  35.0  70000.0    Delhi
+# 3  40.0  80000.0  Chennai
+# 4  32.5  90000.0   Mumbai
+
+# After One-Hot Encoding Categorical Data:
+#      Age   Salary     City  City_Chennai  City_Delhi  City_Mumbai
+# 0  25.0  50000.0    Delhi         False        True        False
+# 1  30.0  60000.0   Mumbai         False       False         True
+# 2  35.0  70000.0    Delhi         False        True        False
+# 3  40.0  80000.0  Chennai          True       False        False
+# 4  32.5  90000.0   Mumbai         False       False         True
